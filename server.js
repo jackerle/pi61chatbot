aef8095542256d9f22ff8a3d6599216bfe3366e9
jackerle <zeustololisis@gmail.com>
2018-10-20 15:05:46 +0700

    20/10/2018

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
+'use strict';
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 const express = require('express')
 const line = require('@line/bot-sdk');
 const app = express()
 require('dotenv').config();
+//const {Conv} = require('./fullfilment');
+const bodyParser = require('body-parser');
+require('actions-on-google')
 
-const config = {
+/*const config = {
   channelAccessToken: process.env.channelAccessToken,
   channelSecret: process.env.channelSecret
 };
 
-const client = new line.Client(config);
+const client = new line.Client(config);*/
+function WebhookProcesing(req, res) {
+  const agent = new WebhookClient({ request: req, response: res });
+  console.info('agent set');
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  agent.handleRequest(intentMap);
+}
+function welcome(agent) {
+  agent.add("Success Congrate!");
+}
+app.post('/api', bodyParser.json(), (req, res) => {
+  console.log("this here!")
+  WebhookProcesing(req, res)
+
+})
+
+/*app.get('/', (req, res) => {
+  res.send('Hello World')
+})*/
+app.listen(3000, () => {
+  console.log('Start server at po rt 3000.')
+})
 
 
-app.post('/', line.middleware(config), (req, res) => {
+
+
+
+/*app.post('/', line.middleware(config), (req, res) => {
   Promise
       .all(req.body.events.map(handleEvent))
       .then((result) => res.json(result));
@@ -35,10 +67,4 @@ function handleMessageEvent(event) {
 
   return client.replyMessage(event.replyToken, msg);
 }
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-app.listen(3000, () => {
-  console.log('Start server at po rt 3000.')
-})
+*/
